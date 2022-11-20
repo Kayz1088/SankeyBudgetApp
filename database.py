6056fce8ff50351873a9fba2fae7ebe53b18cde7
kenrick campbell <kennyg8810@gmail.com>
2022-11-20 11:46:06 -0500

    first commit

---
+++ database.py
@@ -0,0 +1,29 @@
+import os
+
+from deta import Deta
+from dotenv import load_dotenv
+
+
+load_dotenv(".env")
+
+DETA_KEY = os.getenv("DETA_KEY")
+deta = Deta(DETA_KEY)
+
+db = deta.Base("monthly_reports")
+
+
+
+def insert_period(period, incomes, expenses, comment):
+    """Returns the report on a sucessful creation, otherwise raises an error"""
+    return db.put({"key": period, "incomes": incomes, "expenses": expenses, "comment": comment})
+
+
+def fetch_all_periods():
+    """returns a dict of all periods"""
+    res = db.fetch()
+    return res.items
+
+
+def get_period(period):
+    """"if not found, the function will return  None"""
+    return db.get(period)
